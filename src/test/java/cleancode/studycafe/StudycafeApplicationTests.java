9c930217ac6f16cb544dc5356336a85a7b8446bf
seotaemin <taeminseo0115@gmail.com>
2024-10-09 16:22:16 +0900

    [24.10.09] setting project

---
+++ src/test/java/cleancode/studycafe/StudycafeApplicationTests.java
@@ -0,0 +1,13 @@
+package cleancode.studycafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
